734fec1cca132622ab0fd4001d868f937dfa46ac
theunal <97359852+theunal@users.noreply.github.com>
2022-04-23 22:58:40 +0300

    computer

--- src/main/java/com/unal/computer/Computer.java
+++ src/main/java/com/unal/computer/Computer.java
@@ -10,7 +10,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
-
+//
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
